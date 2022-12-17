f1ba461e7afadcb1644703529c3f932e107488cb
Ana Alfaiate <anajfalfaiate@gmail.com>
2022-12-17 18:56:03 +0000

    início funções e classe

--- src/Classifier.java
+++ src/Classifier.java
@@ -1,4 +1,27 @@
 
 public class Classifier {
 
+	private MRFT [] arrayMRFTs;
+	private double [] frequencias;
+	//confirmar tipo de dados de array MRFT
+	//array de MRFTs, um para cada valor da classe
+	// array com as frequencias das classes
+	
+	//CONSTRUTOR
+	
+	public Classifier(MRFT [] MRFTs, double[] valores) {
+		super();
+		this.arrayMRFTs = MRFTs;
+		this.frequencias = valores;
+	}
+	
+	//CLASSIFY
+	
+	public int Classify (int [] valoresx) {
+		// dados valores (x1,...,xn) das variáveis 
+		return; //valor da classe mais provável
+	}
+	
+	
+	//calcular frequencia onde? DataSet
 }
