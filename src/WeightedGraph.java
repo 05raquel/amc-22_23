1678150d7e96c4df3c72fcbc93af5017ef663743
Ana Alfaiate <anajfalfaiate@gmail.com>
2022-12-17 18:43:29 +0000

    primeiras impressões

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,4 +1,27 @@
 
 public class WeightedGraph {
-
+	//não direcionado
+	private int dim;
+	private double [][] matrix; 
+	//confirmar o tipo de dados matrix
+	
+	// ADD
+	
+	public void Add (int i, int j, double peso) {
+		matrix[i][j]=peso;
+		matrix[i][j]=peso;
+	}
+	//mudar se mudarmos o tipo de dados do grafo
+	
+	
+	// MST
+	// árvore de extensão de peso maximal
+	
+	//ver lecture 9
+	
+	public /*tree*/ MST() {
+		return; //tree
+		
+	}
+	
 }
