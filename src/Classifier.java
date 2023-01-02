9b01134b7be3344e199ddec28fdc830accbf805c
Ana Alfaiate <anajfalfaiate@gmail.com>
2023-01-02 20:09:08 +0000

    função classify atualizada e normalização do vetorprob

--- src/Classifier.java
+++ src/Classifier.java
@@ -28,7 +28,40 @@ public class Classifier implements Serializable {
 	
 	//CLASSIFY
 	
-	public int Classify (int [] valoresx) {
+	//TODO usar as duas funções
+	public double [] Classify_parcial (int [] valoresx) {
+		if (valoresx.length!= arrayMRFTs[0].getsizeMRFTree()) { //vetor valoresx com o número de características correto
+			throw new IllegalArgumentException(); //argumento inválido
+		}
+		
+		// dados valores (x1,...,xn) das variáveis
+		// P = frequência x P MC (vetor)
+		
+		double [] probstotal = new double [frequencias.length];
+		for (int i=0; i < frequencias.length; i++) {
+			probstotal[i] = frequencias[i]*arrayMRFTs[i].Prob(valoresx); //i é a classe
+			System.out.println("i:" + i);
+		}
+		System.out.println("Probs das classes"+Arrays.toString(probstotal));
+		return probstotal;  //valor da classe mais provável
+	}
+	
+	public int Classifybest (double [] probstotal) {
+		// dados valores (x1,...,xn) das variáveis
+		// P = frequência x P MC (vetor)
+		double [] maxprob = {-1,-1}; //{classe,valor de prob)
+		for (int i=0; i < probstotal.length; i++) {
+			if (probstotal[i]> maxprob[1]) {
+				maxprob[0]=i;
+				maxprob[1]=probstotal[i];
+			}
+		}
+		return (int)maxprob[0];  //valor da classe mais provável
+	}
+	
+	
+	/** A função retorna um objeto com a classe mais provável e um array com a probabilidade de cada classe */
+	public Res Classify (int [] valoresx) {
 		if (valoresx.length!= arrayMRFTs[0].getsizeMRFTree()) { //vetor valoresx com o número de características correto
 			throw new IllegalArgumentException(); //argumento inválido
 		}
@@ -47,9 +80,25 @@ public class Classifier implements Serializable {
 			}
 		}
 		System.out.println("Probs das classes"+Arrays.toString(probstotal));
-		return (int)maxprob[0];  //valor da classe mais provável
+		return new Res((int)maxprob[0], probstotal);  //valor da classe mais provável
 	}
-
+	
+	public static final class Res {
+		final double [] probstotal;
+		final int bestClass;
+		public Res(int bestClass, double[] probstotal) {
+			this.probstotal = probstotal;
+			this.bestClass = bestClass;
+		}
+		public double[] getProbstotal() {
+			return probstotal;
+		}
+		public int getBestClass() {
+			return bestClass;
+		}
+	}
+	
+	
 	public static Classifier openclf(String filename) throws IOException, ClassNotFoundException {
 		FileInputStream f = new FileInputStream(filename);
 		ObjectInputStream o = new ObjectInputStream(f);
@@ -59,17 +108,18 @@ public class Classifier implements Serializable {
 		return classifier;
 	}
 	
-//	 Mostrar probstotal - normalizado
-	 
-//	public static NormProb  ()
-//	double soma=0;
-//		for (int i=0; i<frequencias.length; i++) {
-//			soma+= probstotal[i];
-//		}
-//		double [] probfinal = new double [frequencias.length];
-//			for (int i=0; i<frequencias.length; i++) {
-//   	probfinal[i]= probstotal[i]/soma;
-//   }
+	/** Recebe o vetor de probabilidades das classes e retorna-o normalizado*/
+	public static double[] NormProb(double [] prob) {
+		double soma=0;
+			for (int i=0; i < prob.length; i++) {
+				soma+= prob[i];
+			}
+		double [] probfinal = new double [prob.length];
+			for (int i=0; i<prob.length; i++) {
+				probfinal[i]= prob[i]/soma;
+			}
+		return probfinal;
+	}
 //   imprimir probfinal - label
 //   Imprimir maxprob[0] - resposta final - label
 }
